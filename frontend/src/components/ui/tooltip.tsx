0bc7ea6f448738c29ca293fae18a09428cd40701
Tung Doan <tungdoan@emddi.com>
2026-02-02 15:24:35 +0700

    fix(tooltip): update TooltipArrow class for improved styling consistency

--- frontend/src/components/ui/tooltip.tsx
+++ frontend/src/components/ui/tooltip.tsx
@@ -47,7 +47,7 @@ function TooltipContent({
         sideOffset={sideOffset}
         {...props}>
         {children}
-        <TooltipPrimitive.Arrow className='bg-foreground fill-foreground z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] data-[side=left]:translate-x-[-1.5px] data-[side=right]:translate-x-[1.5px]' />
+        <TooltipPrimitive.Arrow className='bg-foreground fill-foreground z-50 size-2.5 translate-y-[calc(-50%-2px)] rotate-45 rounded-xs data-[side=left]:translate-x-[-1.5px] data-[side=right]:translate-x-[1.5px]' />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   )
